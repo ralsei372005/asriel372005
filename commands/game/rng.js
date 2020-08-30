796b5c3a55a813f87af3a2194e10cc663e5f65c8
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-08-30 16:33:15 +0700

    Update rng.js

--- commands/game/rng.js
+++ commands/game/rng.js
@@ -1,11 +1,34 @@
-module.exports = {
-	alts: ['random'],
-	args: [2, '<Begin> <End>'],
-	info: 'Random Number Generator! Generate Random Number Between <Begin> and <End>',
-	run () {
-		const {message, args, embed} = require('../index');
-		const $1 = parseInt(args[1]), $2 = parseInt(args[2]);
-		embed.setTitle(Math.floor(Math.random() * ($2 - $1 + 1)) + $1);
-		message.channel.send(embed);
+const {Command} = require('discord.js-commando');
+
+module.exports = class rng extends Command {
+	constructor (client) {
+		super(client, {
+			name: 'rng',
+			group: 'game',
+			memberName: 'rgn',
+			description: 'Random Number Generator! Generate Random Number range <begin>..<end>',
+			throttling: {
+				usages: 2,
+				duration: 10
+			},
+			args: [
+				{
+					key: 'range',
+					prompt: 'Number Range? (Syntax: **`<begin>..<end>`**)',
+					type: 'string',
+					validate: range => {
+						return /^\d+..\d+$/.test(range);
+					}
+				}
+			]
+		});
+	}
+
+	run (message, {range}) {
+		const {embed} = require('../../index');
+		const begin = parseInt(range.split('..')[0]);
+		const end = parseInt(range.split('..')[0]);
+		embed.setTitle(Math.floor(Math.random() * (end - begin + 1)) + begin);
+		message.say(embed);
 	}
 };
