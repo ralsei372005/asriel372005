572d4e377ab204d0142c4a336707dc1e1474429a
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-05 22:22:37 +0700

    Update mc.js

--- commands/changelog/mc.js
+++ commands/changelog/mc.js
@@ -31,7 +31,32 @@ module.exports = class version extends Command {
 			setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true })).
 			setImage('https://i.imgur.com/sH4O8ci.png').
 			setTitle('Minecraft Caves & Cliffs\nMinecraft: Java Edition - 1.17').
-			setDescription('Yes, yes. It’s happening. All of you proved once and for all that enough commenting, pleading, and campaigning will get you anything. As long as that thing is an update specifically to the cave and mountain systems in a game made of blocks. The Caves & Cliffs Update as it is now officially named comes with improved cave generation and features things like lush caves and dripstone caves. There will also be a brand new archeology system, which adds a very cool sense of history and storytelling element to the game. You’re digging now, but soon you’re going to be excavating.\nSomething has to populate these caverns, so we also threw in crystals, telescopes, bundles, the sculk sensor block and a new hostile mob – the warden! Does Minecraft really need more hostile mobs? Isn’t it enough that the llamas spit at you?\nThe cliff in Caves & Cliffs means that the Mountain Update that you voted for last year will be included in this one. We got a first look at the mountain goat that will populate these peaks, and I like it even though I think it resembles me a little too much. I’m not saying a certain Dutch pixel artist did this on purpose, but if he did then he got his revenge. You win this round, Boerstra.\nThe Caves & Cliffs Update also brings you copper! What’s so cool about copper that it gets its own paragraph? Besides being old-timey slang for officers of the law (unless you are British, in which case it is now-timey), copper will add an aging element to the game. No, it does not wrinkle and since we’re on the subject, neither do I. Over time however, your copper builds will start to turn green. So if you’ve been playing Minecraft for a long time, you’re finally getting the cold, hard evidence you deserve and no longer have to rely on insisting that you’re not lying. \nWhat else, what else? I feel like I’m forgetting something… oh yeah! This update brings a new contender for cutest mob, the axolotl! Look at it! It comes in a bucket! A BUCKET!\nHow am I supposed to concentrate on all the other announcements now? They better be good, or I’m going to abandon all of you by throwing myself into a bucket of axolotls. ')
+			addFields([
+				{
+					name: '\u200b',
+					value: 'Yes, yes. It’s happening. All of you proved once and for all that enough commenting, pleading, and campaigning will get you anything. As long as that thing is an update specifically to the cave and mountain systems in a game made of blocks. The Caves & Cliffs Update as it is now officially named comes with improved cave generation and features things like lush caves and dripstone caves. There will also be a brand new archeology system, which adds a very cool sense of history and storytelling element to the game. You’re digging now, but soon you’re going to be excavating.'
+				},
+				{
+					name: '\u200b',
+					value: 'Something has to populate these caverns, so we also threw in crystals, telescopes, bundles, the sculk sensor block and a new hostile mob – the warden! Does Minecraft really need more hostile mobs? Isn’t it enough that the llamas spit at you?'
+				},
+				{
+					name: '\u200b',
+					value: 'The cliff in Caves & Cliffs means that the Mountain Update that you voted for last year will be included in this one. We got a first look at the mountain goat that will populate these peaks, and I like it even though I think it resembles me a little too much. I’m not saying a certain Dutch pixel artist did this on purpose, but if he did then he got his revenge. You win this round, Boerstra.'
+				},
+				{
+					name: '\u200b',
+					value: 'The Caves & Cliffs Update also brings you copper! What’s so cool about copper that it gets its own paragraph? Besides being old-timey slang for officers of the law (unless you are British, in which case it is now-timey), copper will add an aging element to the game. No, it does not wrinkle and since we’re on the subject, neither do I. Over time however, your copper builds will start to turn green. So if you’ve been playing Minecraft for a long time, you’re finally getting the cold, hard evidence you deserve and no longer have to rely on insisting that you’re not lying.'
+				},
+				{
+					name: '\u200b',
+					value: 'What else, what else? I feel like I’m forgetting something… oh yeah! This update brings a new contender for cutest mob, the axolotl! Look at it! It comes in a bucket! A BUCKET!'
+				},
+				{
+					name: '\u200b',
+					value: 'How am I supposed to concentrate on all the other announcements now? They better be good, or I’m going to abandon all of you by throwing myself into a bucket of axolotls.'
+				}
+			])
 		);
 	}
 };
