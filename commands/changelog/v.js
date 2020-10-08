7a54a116946c4cc96d720520fd58f030e5f50699
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-08 12:55:31 +0700

    2020.10.8@13:00
    Indentation tab > 4 spaces
    gitignore is no longer needed so deleted

--- commands/changelog/v.js
+++ commands/changelog/v.js
@@ -4,32 +4,32 @@ const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class version extends Command {
-	constructor (client) {
-		super (client, {
-			name: 'version',
-			aliases: ['ver', 'v'],
-			group: 'changelog',
-			memberName: 'mc',
-			description: 'Version & Change Log!',
-			throttling: {
-				usages: 1,
-				duration: 3600
-			}
-		});
-	}
-	run (message) {
-		message.say(new MessageEmbed().
-			setTimestamp().
-			setColor('#ff0000').
-			setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true })).
-			setTitle('Version: 2020.10.4').
-			setDescription('Made by: hiccup372005').
-			addFields(
-				{
-					name: 'Change Logs',
-					value: 'default prefix: "t3"\nRe-add t3mc for potential minecraft 1.17 Caves & Cliffs update.\n~~Glitch~~ is no more, Heroku sucks according to ChrisCJ and UK(nown) is using Heroku, Entering Repl.it'
-				}
-			)
-		);
-	}
+    constructor (client) {
+        super (client, {
+            name: 'version',
+            aliases: ['ver', 'v'],
+            group: 'changelog',
+            memberName: 'mc',
+            description: 'Version & Change Log!',
+            throttling: {
+                usages: 1,
+                duration: 3600
+            }
+        });
+    }
+    run (message) {
+        message.say(new MessageEmbed().
+            setTimestamp().
+            setColor('#ff0000').
+            setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true })).
+            setTitle('Version: 2020.10.4').
+            setDescription('Made by: hiccup372005').
+            addFields(
+                {
+                    name: 'Change Logs',
+                    value: 'default prefix: "t3"\nRe-add t3mc for potential minecraft 1.17 Caves & Cliffs update.\n~~Glitch~~ is no more, Heroku sucks according to ChrisCJ and UK(nown) is using Heroku, Entering Repl.it'
+                }
+            )
+        );
+    }
 };
--- index.js
+++ index.js
@@ -3,30 +3,29 @@ const path = require('path');
 const { CommandoClient } = require('discord.js-commando');
 
 const client = new CommandoClient({
-	commandPrefix: 't3',
-	owner: '379643682984296448'
+    commandPrefix: 't3',
+    owner: '379643682984296448'
 });
 
 client.registry.
-	registerDefaultTypes().
-	registerGroups([
-		['utility', 'Utilities!'],
-		['changelog', 'Version & Change Log!'],
-		['moderation', 'Moderations Commands!']
-	]).
-	registerDefaultGroups().
-	registerDefaultCommands().
-	registerCommandsIn(path.join(__dirname, 'commands'));
+    registerDefaultTypes().
+    registerGroups([
+        ['utility', 'Utilities!'],
+        ['changelog', 'Version & Change Log!'],
+        ['moderation', 'Moderations Commands!']
+    ]).
+    registerDefaultGroups().
+    registerDefaultCommands().
+    registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
-	console.log('ready');
-	client.user.setActivity('t3help | hiccup372005\'s Discord Bot');
+    console.log('ready');
+    client.user.setActivity('t3help | hiccup372005\'s Discord Bot');
 });
 
 client.login(process.env.TOKEN);
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200);
-	res.end('ok');
-});
-server.listen(3000);
+http.createServer((_request, respond) => {
+    respond.writeHead(200);
+    respond.end('ok');
+}).listen(3000);
