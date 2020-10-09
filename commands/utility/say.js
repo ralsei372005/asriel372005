a4c31e958789e2d52830f0ad93b387f9e6c203b9
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-09 21:14:21 +0700

    Create say.js

---
+++ commands/utility/say.js
@@ -0,0 +1,38 @@
+//* v2020.10.9
+
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class say extends Command {
+    constructor (client) {
+        super (client, {
+            name: 'say',
+            group: 'utility',
+            memberName: 'say',
+            description: 'Say a message & I will say it!',
+            throttling: {
+                usages: 3,
+                duration: 30
+            },
+            args: [
+                {
+                    key: 'arg',
+                    prompt: 'Message?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+    run (message, { arg }) {
+        const embed = new MessageEmbed().
+            setTimestamp().
+            setColor('#ff0000').
+            setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }));
+        if (arg.length < 256) {
+            embed.setTitle(arg);
+        } else {
+            embed.setDescription(arg);
+        }
+        message.say(embed);
+    }
+};
