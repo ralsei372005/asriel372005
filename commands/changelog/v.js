cc43b511ee97101e48ac7e4db9a5d07ba974c3f5
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-09 18:45:04 +0700

    update changelog
    @18:45

--- commands/changelog/v.js
+++ commands/changelog/v.js
@@ -1,4 +1,4 @@
-// * v2020.10.4
+//* v2020.10.9
 
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
@@ -9,8 +9,8 @@ module.exports = class version extends Command {
             name: 'version',
             aliases: ['ver', 'v'],
             group: 'changelog',
-            memberName: 'mc',
-            description: 'Version & Change Log!',
+            memberName: 'v',
+            description: 'Version & Owner!',
             throttling: {
                 usages: 1,
                 duration: 3600
@@ -22,14 +22,8 @@ module.exports = class version extends Command {
             setTimestamp().
             setColor('#ff0000').
             setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true })).
-            setTitle('Version: 2020.10.4').
-            setDescription('Made by: hiccup372005').
-            addFields(
-                {
-                    name: 'Change Logs',
-                    value: 'default prefix: "t3"\nRe-add t3mc for potential minecraft 1.17 Caves & Cliffs update.\n~~Glitch~~ is no more, Heroku sucks according to ChrisCJ and UK(nown) is using Heroku, Entering Repl.it'
-                }
-            )
+            setTitle('Version: v2020.10.9').
+            setDescription('Owner: hiccup372005')
         );
     }
 };
