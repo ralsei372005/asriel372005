4c495365d166148ea3c8eda6d1e68b61187968c9
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-09 21:43:23 +0700

    Update rng.js

--- commands/utility/rng.js
+++ commands/utility/rng.js
@@ -45,7 +45,7 @@ module.exports = class rng extends Command {
 function toEmoji (s) {
     let title;
     const emoji = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
-    for (const c in toString(s)) {
+    for (const c of toString(s)) {
         title += emoji[c];
     }
     return title;
