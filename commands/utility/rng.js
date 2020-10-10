f3734c8bc081f7bfeda4faa758748ef766ede710
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-10-10 08:56:41 +0700

    Update rng.js
    v2020.10.10@09:00

--- commands/utility/rng.js
+++ commands/utility/rng.js
@@ -37,7 +37,7 @@ module.exports = class rng extends Command {
             setTimestamp().
             setColor('#ff0000').
             setAuthor(message.author.tag, message.author.avatarURL({ format: 'png', dynamic: true })).
-            setTitle(Array.from((0 | (end - begin + 1) * Math.random() + begin).toString().split(''), x => ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'][0 | x]).join(''))
+            setTitle(Array.from((0 | (end - begin + 1) * Math.random() + begin).toString().split(''), x => ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'][x]).join(''))
         );
     }
 };
