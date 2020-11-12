51da4f5a03546a09b153fc10e5356d94996ffe7d
hiccup372005 <64270699+hiccup372005@users.noreply.github.com>
2020-11-12 16:55:08 +0700

    Update v.js

--- commands/changelog/v.js
+++ commands/changelog/v.js
@@ -8,7 +8,7 @@ module.exports = class v extends Command {
 			aliases: ["ver", "v"],
 			group: "changelog",
 			memberName: "v",
-			description: "Version & Owner!",
+			description: "Version & Developer",
 			throttling: { usages: 1, duration: 3600 }
 		});
 	}
@@ -17,11 +17,11 @@ module.exports = class v extends Command {
 			.setTimestamp()
 			.setColor("#ff0000")
 			.setAuthor(message.author.tag, message.author.displayAvatarURL({ format: "png", dynamic: true }))
-			.setTitle("Version: v2020.10.18")
-			.setDescription("Owner: hiccup372005")
+			.setTitle("Version: 2020.46")
+			.setDescription("Developer: hiccup372005")
 			.addFields({
 				name: "Change Log",
-				value: "Due to a request of my friend, added BigNum/BigInt support for the random number generator command! `t3rng 0..100000000000000000000000000000000000000000000000000` will no longer return negative value because of Integer Overflow."
+				value: "tags will be back! with a cost...\n You see, to me personally repl.it is way better than heroku, mostly because heroku doesn't have the save file on disk concept. I used to store the files locally with sequelize and sqlite. Repl.it, however, every repl is public, every files is public except for the .env file which I store the discord bot token. If you want to use (create) tags, you must accept the fact that your user ID, tag name, tag description might be seen. No username or nickname will be stored, just user ID. DM me what you think."
 			})
 		);
 	}
