47d4276a15bdaeeb2ef68fbaeea1864ff5b38172
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-04-30 15:34:11 +0700

    Update to actually not to ping in #hydra (previously it was ping in #hydra and not to ping in other channel...)

--- index.js
+++ index.js
@@ -39,12 +39,10 @@ client2.login(process.env.TOKEN);
 client2.once('ready', () => console.log('Discord.js ✅'));
 
 client2.on('message', message => {
-	if(message.author.id !== '793883534983954454') return;
-	if(message.channel.id !== '833688031315230721') return;
+	if(message.author.id !== '793883534983954454' || message.channel.id === '833688031315230721') return;
 	message.channel.send(`<@379643682984296448>`)
-		.then(message2 => setTimeout(()=>message2.delete(),3000))
+		.then(message2 => setTimeout(()=>message2.delete(),1000))
 		.catch(console.error);
-	console.log(message.content);
 });
 
 /*  AnIdiots.Guide  */
