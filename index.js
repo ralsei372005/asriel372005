9a36823796b48c33b135f0a3f3ec5f2148e3c246
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-05-04 17:55:21 +0700

    Update Index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client2 = new discord_js_1.Client()
 client2.login(process.env.TOKEN)
 client2.once('ready', function () { return console.log('Discord.js ✅') })
 client2.on('message', function (message) {
-  if (message.author.id !== '793883534983954454' || message.channel.id === '833688031315230721') { return }
+  if (message.author.id !== '793883534983954454' || message.channel.id === process.env.CHANNEL) { return }
   message.channel.send('<@379643682984296448>')
     .then(function (message2) { return setTimeout(function () { return message2.delete() }, 1000) }).catch(console.error)
 })
