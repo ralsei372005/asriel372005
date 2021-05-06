11645175c3df5944e97230bdc2b79324a9d8a8ac
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-05-06 20:37:21 +0700

    Ditch hard code static Channel ID in favor of process.env

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_commando_1 = require("discord.js-commando");
-const discord_js_1 = require("discord.js");
-const dotenv = require("dotenv");
-const http = require("http");
-const path = require("path");
+exports.__esModule = true;
+var discord_js_commando_1 = require("discord.js-commando");
+var discord_js_1 = require("discord.js");
+var dotenv = require("dotenv");
+var http = require("http");
+var path = require("path");
 dotenv.config();
 // Commando
-const client = new discord_js_commando_1.CommandoClient({
+var client = new discord_js_commando_1.CommandoClient({
     commandPrefix: 'asriel',
     owner: '379643682984296448'
 });
 client.login(process.env.TOKEN);
-client.once('ready', () => console.log('Commando ✅'));
+client.once('ready', function () { return console.log('Commando ✅'); });
 client.registry
     .registerDefaultTypes()
     .registerGroups([
@@ -24,18 +24,17 @@ client.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 // Discord.js
-const client2 = new discord_js_1.Client();
+var client2 = new discord_js_1.Client();
 client2.login(process.env.TOKEN);
-client2.once('ready', () => console.log('Discord.js ✅'));
-client2.on('message', message => {
-    if (message.author.id !== '793883534983954454' || message.channel.id === '833688031315230721')
+client2.once('ready', function () { return console.log('Discord.js ✅'); });
+client2.on('message', function (message) {
+    if (message.author.id !== '793883534983954454' || message.channel.id === process.env.CHANNEL)
         return;
     message.channel.send('<@379643682984296448>')
-        .then(message2 => setTimeout(() => message2.delete(), 1000))
-        .catch(console.error);
+        .then(function (message2) { return setTimeout(function () { return message2["delete"](); }, 1000); })["catch"](console.error);
 });
 // AnIdiots.Guide
-http.createServer((_request, respond) => {
+http.createServer(function (_request, respond) {
     respond.writeHead(200);
     respond.end('ok');
 }).listen(3000);
