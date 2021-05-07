8c3d4a7805fd4d2bfa9f430da401ce5678f939ea
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-05-07 15:14:16 +0700

    Changes to pfp.js, including changes to member finding piority, with displayNam being the highest and username starts with being the lowest and pfp size 4096

--- commands/general/pfp.js
+++ commands/general/pfp.js
@@ -8,6 +8,7 @@ module.exports = class pfp extends Command {
       group: 'general',
       memberName: 'pfp',
       description: 'Profile Picture',
+      guildOnly: true,
       args: [
         {
           key: 'arg',
@@ -24,19 +25,28 @@ module.exports = class pfp extends Command {
       return message.say(new MessageEmbed()
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
         .setColor('#00ff00')
-        .setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true }))
+        .setTitle(`${message.author.tag}'s Profile Picture`)
+        .setImage(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
         .setFooter('Ralsei372005\'s Discord Bot', process.env.PFP)
         .setTimestamp()
       )
     }
-    const user = this.client.users.cache.find(user2 => user2.username.startsWith(arg))
-    if (!user) return message.say(`No user with username starts with ${arg} found.`)
-    return message.say(new MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
-      .setColor('#00ff00')
-      .setImage(user.displayAvatarURL({ format: 'png', dynamic: true }))
-      .setFooter('Ralsei372005\'s Discord Bot', process.env.PFP)
-      .setTimestamp()
-    )
+    const member =
+      message.guild.members.cache.find(member2 => member2.displayName === arg) ||
+      message.guild.members.cache.find(member2 => member2.user.tag === arg) ||
+      message.guild.members.cache.find(member2 => member2.user.username === arg) ||
+      message.guild.members.cache.find(member2 => member2.displayName.startsWith(arg)) ||
+      message.guild.members.cache.find(member2 => member2.user.tag.startsWith(arg)) ||
+      message.guild.members.cache.find(member2 => member2.user.username.startsWith(arg))
+    if (member) {
+      return message.say(new MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true }))
+        .setColor('#00ff00')
+        .setTitle(`${member.user.tag}'s Profile Picture`)
+        .setImage(member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
+        .setFooter('Ralsei372005\'s Discord Bot', process.env.PFP)
+        .setTimestamp()
+      )
+    }
   }
 }
