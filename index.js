8c5527ecc663d04143d5f9e3b4dde9d59f8e70d3
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-05-07 16:15:25 +0700

    ditch logger.log(log.level,log.message) in favor of logger.info(log.message)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const client = new CommandoClient({
   owner: '379643682984296448'
 })
 client.login(process.env.TOKEN)
-client.once('ready', () => logger.log(2, 'Commando ✅'))
+client.once('ready', () => logger.info('Commando ✅'))
 client.registry
   .registerDefaultTypes()
   .registerGroups([
@@ -32,7 +32,7 @@ client.registry
 const { Client } = require('discord.js')
 const client2 = new Client()
 client2.login(process.env.TOKEN)
-client2.once('ready', () => logger.log(2, 'Discord.js ✅'))
+client2.once('ready', () => logger.info('Discord.js ✅'))
 client2.on('message', message => {
   if (message.author.id !== '793883534983954454' || message.channel.id === process.env.CHANNEL) return
   message.channel.send('<@379643682984296448>').then(message2 => setTimeout(message2.delete(), 1000))
