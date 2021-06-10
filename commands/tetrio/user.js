1447451b7b42afa6a472540d7e56994cbcc0d7b0
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-06-10 20:40:16 +0700

    Update commands/tetrio/user.js and add records (40lines & blitz) fields

--- commands/tetrio/user.js
+++ commands/tetrio/user.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const {version} = require('../../package.json');
-const {pfp, ranks} = require('../../config.json');
+const {pfp, ranks, emojis} = require('../../config.json');
 const {Command} = require('discord.js-commando');
 const {MessageEmbed} = require('discord.js');
 
@@ -10,45 +10,73 @@ module.exports = class user extends Command {
 			name: 'user',
 			group: 'tetrio',
 			memberName: 'user',
-			description: 'TETR.IO user',
+			description: 'TETR.IO User Info & Records',
 			args: [{
-				key: 'user',
-				prompt: 'TETR.IO username?',
+				key: 'username',
+				prompt: 'TETR.IO Username?',
 				type: 'string',
-				validate: user => /^[a-zA-Z0-9-_]{3,16}$/.test(user)
+				validate: username => /^[a-zA-Z0-9-_]{3,16}$/.test(username)
 			}]
 		});
 	}
 
-	async run(message, {arg}) {
-		const response = await fetch(`https://ch.tetr.io/api/users/${arg}`).then(response => response.json());
-		if (!response.success) {
-			return message.say(response.error);
-		}
-
-		const {user, user: {league}} = response.data;
-		message.say(new MessageEmbed()
+	async run(message, {username}) {
+		username = username.toLowerCase();
+		const embed = new MessageEmbed()
 			.setAuthor(message.author.tag, message.author.displayAvatarURL({format: 'png', dynamic: true}))
 			.setColor('#00ff00')
-			.setTitle(`${user}'s TETR.IO`)
-			.setURL(`https://ch.tetr.io/u/${user}`)
 			.setFooter(`Ralsei372005's Discord Bot Version ${version}`, pfp)
-			.setTimestamp()
+			.setTimestamp();
+
+		const info = await fetch(`https://ch.tetr.io/api/users/${username}`).then(response => response.json());
+		if (!info.success) {
+			embed.setTitle(info.error);
+			return message.say(embed);
+		}
+
+		const records = await fetch(`https://ch.tetr.io/api/users/${username}/records`).then(response => response.json());
+		if (!records.success) {
+			embed.setTitle(records.error);
+			return message.say(embed);
+		}
+
+		const {data: {user, user: {league}}} = info;
+		const finalTime = records.data.records['40l'].record?.endcontext.finalTime;
+		const score = records.data.records.blitz.record?.endcontext.score;
+		embed
+			.setTitle(username)
+			.setURL(`https://ch.tetr.io/u/${username}`)
 			.setThumbnail(`https://tetr.io/user-content/avatars/${user._id}.jpg?rv=${user.avatar_revision}`)
 			.addFields([
-				{name: 'Online Games Played', value: user.gamesplayed, inline: true},
-				{name: 'Online Games Won', value: user.gameswon, inline: true},
+				{name: 'Online Games Played', value: toEmojis(user.gamesplayed), inline: true},
+				{name: 'Online Games Won', value: toEmojis(user.gameswon), inline: true},
 				{name: '\u200B', value: 'TETRA LEAGUE'},
-				{name: 'Games Played', value: league.gamesplayed, inline: true},
-				{name: 'Games Won', value: league.gameswon, inline: true},
+				{name: 'Games Played', value: toEmojis(league.gamesplayed), inline: true},
+				{name: 'Games Won', value: toEmojis(league.gameswon), inline: true},
 				{name: '\u200B', value: '\u200B', inline: true},
-				{name: 'Rating', value: `${league.rating.toFixed?.(2)} TR`, inline: true},
-				{name: 'Glicko', value: `${league.glicko.toFixed?.(2)} ± ${league.rd?.toFixed?.(2)} RD`, inline: true},
+				{name: 'Rating', value: `${toEmojis(league.rating?.toFixed(0))} TR`, inline: true},
+				{name: 'Glicko', value: `${toEmojis(league.glicko?.toFixed(0))} ± ${toEmojis(league.rd?.toFixed(0))}`, inline: true},
 				{name: 'Rank', value: ranks[league.rank], inline: true},
-				{name: 'Attack Per Minute', value: league.apm, inline: true},
-				{name: 'Pieces Per Second', value: league.pps, inline: true},
-				{name: 'Versus Score', value: league.vs, inline: true}
-			])
-		);
+				{name: 'Attack Per Minute', value: toEmojis(league.apm), inline: true},
+				{name: 'Pieces Per Second', value: toEmojis(league.pps), inline: true},
+				{name: 'Versus Score', value: toEmojis(league.vs), inline: true},
+				{name: '\u200B', value: 'RECORDS'},
+				{name: '40 Lines', value: toEmojis(`${(finalTime / 60000).toFixed(0)}:${finalTime % 60000 < 10000 ? '0' : ''}${(finalTime % 60000 / 1000).toFixed(3)}`), inline: true},
+				{name: 'Blitz', value: toEmojis(score), inline: true}
+			]);
+		message.say(embed);
 	}
 };
+
+function toEmojis(arg) {
+	if (!arg) {
+		return arg;
+	}
+
+	let string = '';
+	for (const char of arg.toString()) {
+		string += emojis[char];
+	}
+
+	return string;
+}
