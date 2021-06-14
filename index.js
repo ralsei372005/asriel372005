930c3f2f3323a2e0078929f6040c370bc96f94cc
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-06-14 21:17:59 +0700

    Part 1 of ? : Reverting object destructing spamming abuse

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 require('dotenv').config();
+
 const {version} = require('./package.json');
 const {commandPrefix, owner, groups} = require('./config.json');
+
 const {CommandoClient} = require('discord.js-commando');
 const {join} = require('path');
-const {log} = console;
 
-const {registry: {registerDefaultTypes, registerGroups, registerDefaultGroups, registerDefaultCommands, registerCommandsIn}, once, login, user: {setActivity}} = new CommandoClient({
+const client = new CommandoClient({
 	commandPrefix,
 	owner
 });
 
-registerDefaultTypes();
-registerGroups(groups);
-registerDefaultGroups();
-registerDefaultCommands();
-registerCommandsIn(join(__dirname, 'commands'));
+client.registry
+	.registerDefaultTypes()
+	.registerGroups(groups)
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(join(__dirname, './commands'));
 
-once('ready', () => {
-	log('✅');
-	setActivity(`ralsei372005's Discord Bot Version ${version}`);
+client.once('ready', () => {
+	console.log(`ralsei372005's Discord Bot Version ${version}`);
+	client.user.setActivity(`ralsei372005's Discord Bot Version ${version}`);
 });
 
-login(process.env.token);
+client.on('error', console.error);
 
-const {createServer} = require('http');
-createServer((_request, response) => {
-	const {writeHead, end} = response;
-	writeHead(200);
-	end(`ralsei372005's Discord Bot Version ${version}`);
-}).listen(3000);
+client.login(process.env.token);
+
+require('https').createServer((_request, response) => {
+	response.writeHead(200);
+	response.end(`ralsei372005's Discord Bot Version ${version}`);
+});
