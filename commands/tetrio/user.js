ed00ddf6ab4b3a4ac699b6581d79932e9c917959
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-06-15 14:08:11 +0700

    Update commands/tetrio/user.js to comply with the previous commit rules about object destructing. On that note, there's some cases where destructing does not help reducing a variable, but still helps a lot with cleaning multiple chaining . and therefore still being destructed. Notable example including object destructing embed because it cleans a lot of embed. from the old code

--- commands/tetrio/user.js
+++ commands/tetrio/user.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
+
 const {version} = require('../../package.json');
 const {pfp, ranks, emojis} = require('../../config.json');
+
 const {Command} = require('discord.js-commando');
 const {MessageEmbed} = require('discord.js');
 
@@ -22,56 +24,60 @@ module.exports = class user extends Command {
 
 	async run(message, {username}) {
 		username = username.toLowerCase();
-		const embed = new MessageEmbed()
-			.setAuthor(message.author.tag, message.author.displayAvatarURL({format: 'png', dynamic: true}))
-			.setColor('#00ff00')
-			.setFooter(`Ralsei372005's Discord Bot Version ${version}`, pfp)
-			.setTimestamp();
+
+		const embed = new MessageEmbed();
+		const {setAuthor, setColor, setFooter, setTimestamp, setTitle, setURL, setThumbnail, addField} = embed;
+
+		setAuthor(message.author.tag, message.author.displayAvatarURL({format: 'png', dynamic: true}));
+		setColor('#00ff00');
+		setFooter(`Ralsei372005's Discord Bot Version ${version}`, pfp);
+		setTimestamp();
 
 		const info = await fetch(`https://ch.tetr.io/api/users/${username}`).then(response => response.json());
 		if (!info.success) {
-			embed.setTitle(info.error);
+			setTitle(info.error);
 			return message.say(embed);
 		}
 
 		const records = await fetch(`https://ch.tetr.io/api/users/${username}/records`).then(response => response.json());
 		if (!records.success) {
-			embed.setTitle(records.error);
+			setTitle(records.error);
 			return message.say(embed);
 		}
 
-		const {data: {user, user: {league}}} = info;
+		const {data: {user, user: {league, league: {rating, glicko, rank, apm, pps, vs}}}} = info;
 
-		const score = records.data.records.blitz.record && records.data.records.blitz.record.endcontext.score;
-		embed
-			.setTitle(username)
-			.setURL(`https://ch.tetr.io/u/${username}`)
-			.setThumbnail(`https://tetr.io/user-content/avatars/${user._id}.jpg?rv=${user.avatar_revision}`)
-			.addFields([
-				{name: 'Online Games Played', value: toEmojis(user.gamesplayed), inline: true},
-				{name: 'Online Games Won', value: toEmojis(user.gameswon), inline: true},
-				{name: '\u200B', value: 'TETRA LEAGUE'},
-				{name: 'Games Played', value: toEmojis(league.gamesplayed), inline: true},
-				{name: 'Games Won', value: toEmojis(league.gameswon), inline: true},
-				{name: '\u200B', value: '\u200B', inline: true},
-				{name: 'Rating', value: `${toEmojis(league.rating.toFixed(0))} TR`, inline: true},
-				{name: 'Glicko', value: `${toEmojis(league.glicko.toFixed(0))} ± ${toEmojis(league.rd?.toFixed(0))}`, inline: true},
-				{name: 'Rank', value: ranks[league.rank], inline: true},
-				{name: 'Attack Per Minute', value: toEmojis(league.apm), inline: true},
-				{name: 'Pieces Per Second', value: toEmojis(league.pps), inline: true},
-				{name: 'Versus Score', value: toEmojis(league.vs), inline: true}
-			]);
-		if (records.data.records['40l'].record || records.data.records.blitz.record) {
-			embed.addField('\u200B', 'RECORDS');
+		setTitle(username);
+		setURL(`https://ch.tetr.io/u/${username}`);
+		setThumbnail(`https://tetr.io/user-content/avatars/${user._id}.jpg?rv=${user.avatar_revision}`);
+		addField('Online Games Played', toEmojis(user.gamesplayed), true);
+		addField('Online Games Won', toEmojis(user.gameswon), true);
+		addField('\u200B', '\u200B', true);
+		addField('Tetra League Games Played', toEmojis(league.gamesplayed));
+		addField('Tetra League Games Won', toEmojis(league.gameswon));
+		addField('\u200B', '\u200B', true);
+
+		if (rating && glicko && rank) {
+			addField('Rating', rating.toFixed(0), true);
+			addField('Glicko', glicko.toFixed(0), true);
+			addField('Rank', ranks[rank], true);
 		}
 
-		if (records.data.records['40l'].record) {
-			const {data: {records: {'40l': {record: {endcontext: {finalTime}}}}}} = records;
-			embed.addField('40 Lines', toEmojis(`${(finalTime / 60000).toFixed(0)}:${finalTime % 60000 < 10000 ? '0' : ''}${(finalTime % 60000 / 1000).toFixed(3)}`), true);
+		if (apm && pps && vs) {
+			addField('Attack Per Minute', toEmojis(apm), true);
+			addField('Pieces Per Second', toEmojis(pps), true);
+			addField('Versus Score', toEmojis(vs));
+		}
+
+		const finalTime = records.data.records['40l'].record && records.data.records['40l'].record.endcontext.finalTime;
+		const score = records.data.records.blitz.record && records.data.records.blitz.record.endcontext.score;
+
+		if (finalTime) {
+			addField('40 Lines', toEmojis(`${(finalTime / 60000).toFixed(0)}:${finalTime % 60000 < 10000 ? '0' : ''}${(finalTime % 60000 / 1000).toFixed(3)}`), true);
 		}
 
 		if (records.data.records.blitz.record) {
-			embed.addField('Blitz', toEmojis(score), true);
+			addField('Blitz', toEmojis(score), true);
 		}
 
 		message.say(embed);
@@ -79,14 +85,10 @@ module.exports = class user extends Command {
 };
 
 function toEmojis(arg) {
-	if (!arg) {
-		return arg;
-	}
-
-	let string = '';
+	let result = '';
 	for (const char of arg.toString()) {
-		string += emojis[char];
+		result += emojis[char];
 	}
 
-	return string;
+	return result;
 }
