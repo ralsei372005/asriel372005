45ae5e66990b616873042d370175859b343756dc
ralsei372005 <64270699+ralsei372005@users.noreply.github.com>
2021-06-16 10:56:08 +0700

    Update commands/tetrio/user.js command, one optional argument for raw numbers and letters instead of emojis

--- commands/tetrio/user.js
+++ commands/tetrio/user.js
@@ -1,8 +1,6 @@
 const fetch = require('node-fetch');
-
 const {version} = require('../../package.json');
 const {pfp, ranks, emojis} = require('../../config.json');
-
 const {Command} = require('discord.js-commando');
 const {MessageEmbed} = require('discord.js');
 
@@ -18,77 +16,88 @@ module.exports = class user extends Command {
 				prompt: 'TETR.IO Username?',
 				type: 'string',
 				validate: username => /^[a-zA-Z0-9-_]{3,16}$/.test(username)
+			}, {
+				key: 'raw',
+				prompt: '',
+				type: 'string',
+				default: '',
+				oneOf: ['', 'raw']
 			}]
 		});
 	}
 
-	async run(message, {username}) {
-		username = username.toLowerCase();
-
+	async run(message, {username, raw}) {
 		const embed = new MessageEmbed()
 			.setAuthor(message.author.tag, message.author.displayAvatarURL({format: 'png', dynamic: true}))
 			.setColor('#00ff00')
 			.setFooter(`Ralsei372005's Discord Bot Version ${version}`, pfp)
 			.setTimestamp();
 
-		const info = await fetch(`https://ch.tetr.io/api/users/${username}`).then(response => response.json());
+		const info = await fetch(`https://ch.tetr.io/api/users/${username.toLowerCase()}`).then(response => response.json());
 		if (!info.success) {
 			embed.setTitle(info.error);
 			return message.say(embed);
 		}
 
-		const records = await fetch(`https://ch.tetr.io/api/users/${username}/records`).then(response => response.json());
+		const records = await fetch(`https://ch.tetr.io/api/users/${username.toLowerCase()}/records`).then(response => response.json());
 		if (!records.success) {
 			embed.setTitle(records.error);
 			return message.say(embed);
 		}
 
 		const {data: {user, user: {league, league: {rating, glicko, rank, apm, pps, vs}}}} = info;
+		const finalTime = records.data.records['40l'].record && records.data.records['40l'].record.endcontext.finalTime;
+		const score = records.data.records.blitz.record && records.data.records.blitz.record.endcontext.score;
 
 		embed
-			.setTitle(username)
-			.setURL(`https://ch.tetr.io/u/${username}`)
+			.setTitle(toEmojis(username.toUpperCase(), raw))
 			.setThumbnail(`https://tetr.io/user-content/avatars/${user._id}.jpg?rv=${user.avatar_revision}`)
-			.addField('Online Games Played', toEmojis(user.gamesplayed), true)
-			.addField('Online Games Won', toEmojis(user.gameswon), true)
+			.addField('Online Games Played', toEmojis(user.gamesplayed, raw), true)
+			.addField('Online Games Won', toEmojis(user.gameswon, raw), true)
 			.addField('\u200B', '\u200B', true)
-			.addField('Tetra League Games Played', toEmojis(league.gamesplayed), true)
-			.addField('Tetra League Games Won', toEmojis(league.gameswon), true)
+			.addField('Tetra League Games Played', toEmojis(league.gamesplayed, raw), true)
+			.addField('Tetra League Games Won', toEmojis(league.gameswon, raw), true)
 			.addField('\u200B', '\u200B', true);
 
 		if (rating && glicko && rank) {
 			embed
-				.addField('Rating', toEmojis(rating.toFixed(0)), true)
-				.addField('Glicko', toEmojis(glicko.toFixed(0)), true)
+				.addField('Rating', toEmojis(rating.toFixed(0), raw), true)
+				.addField('Glicko', toEmojis(glicko.toFixed(0), raw), true)
 				.addField('Rank', ranks[rank], true);
 		}
 
 		if (apm && pps && vs) {
 			embed
-				.addField('Attack Per Minute', toEmojis(apm), true)
-				.addField('Pieces Per Second', toEmojis(pps), true)
-				.addField('Versus Score', toEmojis(vs), true);
+				.addField('Attack Per Minute', toEmojis(apm, raw), true)
+				.addField('Pieces Per Second', toEmojis(pps, raw), true)
+				.addField('Versus Score', toEmojis(vs, raw), true);
 		}
-
-		const finalTime = records.data.records['40l'].record && records.data.records['40l'].record.endcontext.finalTime;
-		const score = records.data.records.blitz.record && records.data.records.blitz.record.endcontext.score;
 
 		if (finalTime) {
-			embed.addField('40 Lines', toEmojis(`${(finalTime / 60000).toFixed(0)}:${finalTime % 60000 < 10000 ? '0' : ''}${(finalTime % 60000 / 1000).toFixed(3)}`), true);
+			embed.addField('40 Lines', toEmojis(`${(finalTime / 60000).toFixed(0)}:${finalTime % 60000 < 10000 ? '0' : ''}${(finalTime % 60000 / 1000).toFixed(3)}`, raw), true);
 		}
 
-		if (records.data.records.blitz.record) {
-			embed.addField('Blitz', toEmojis(score), true);
+		if (score) {
+			embed.addField('Blitz', toEmojis(score, raw), true);
 		}
 
-		message.say(embed);
+		if (raw) {
+			message.say(embed);
+		} else {
+			message.say(`\`asriel user ${username} raw\` for raw numbers and letters instead of emojis`, {embed});
+		}
 	}
 };
 
-function toEmojis(arg) {
+function toEmojis(arg, raw) {
+	if (raw) {
+		return arg;
+	}
+
 	let result = '';
 	for (const char of arg.toString()) {
 		result += emojis[char];
+		result += ' ';
 	}
 
 	return result;
--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const date = new Date();
 const [year, month, day, hour, minutes, seconds] = [date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
 
 require('dotenv').config();
-
 require('https').createServer((_request, response) => {
+	response.write(`ralsei372005's Discord Bot Version ${version}`);
+	response.write(`${year}.${month}.${day} ${hour}:${minutes}:${seconds}`);
 	response.writeHead(200);
-	response.end(`ralsei372005's Discord Bot Version ${version}\n${year}.${month}.${day} ${hour}:${minutes}:${seconds}`);
+	response.end();
 }).listen(3000);
 
 const {version} = require('./package.json');
 const {commandPrefix, owner, groups} = require('./config.json');
-
 const {CommandoClient} = require('discord.js-commando');
-const {join} = require('path');
-
 const client = new CommandoClient({commandPrefix, owner});
 
 client.login(process.env.token);
-
 client.once('ready', () => {
-	console.log(`ralsei372005's Discord Bot Version ${version}\n${year}.${month}.${day} ${hour}:${minutes}:${seconds}`);
 	client.user.setActivity(`ralsei372005's Discord Bot Version ${version}`);
+	console.log(`ralsei372005's Discord Bot Version ${version}`);
+	console.log(`${year}.${month}.${day} ${hour}:${minutes}:${seconds}`);
 });
 
 client.registry
@@ -28,4 +26,4 @@ client.registry
 	.registerGroups(groups)
 	.registerDefaultGroups()
 	.registerDefaultCommands()
-	.registerCommandsIn(join(__dirname, './commands'));
+	.registerCommandsIn(require('path').join(__dirname, './commands'));
